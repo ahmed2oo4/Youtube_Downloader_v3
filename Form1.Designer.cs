33ac17c698b8b86c743fe242a7a7198de2b1aa04
Ahmed Makram <128056607+ahmed2oo4@users.noreply.github.com>
2024-09-22 13:57:59 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace YouTubeDownloaderApp
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.cmbQuality = new System.Windows.Forms.ComboBox();
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(12, 12);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(400, 20);
+            this.txtUrl.TabIndex = 0;
+            // 
+            // cmbQuality
+            // 
+            this.cmbQuality.FormattingEnabled = true;
+            this.cmbQuality.Location = new System.Drawing.Point(418, 12);
+            this.cmbQuality.Name = "cmbQuality";
+            this.cmbQuality.Size = new System.Drawing.Size(121, 21);
+            this.cmbQuality.TabIndex = 1;
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Location = new System.Drawing.Point(12, 39);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(75, 23);
+            this.btnDownload.TabIndex = 2;
+            this.btnDownload.Text = "Download";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(12, 68);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(527, 23);
+            this.progressBar.TabIndex = 3;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(12, 94);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(37, 13);
+            this.lblStatus.TabIndex = 4;
+            this.lblStatus.Text = "Status";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(551, 122);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.btnDownload);
+            this.Controls.Add(this.cmbQuality);
+            this.Controls.Add(this.txtUrl);
+            this.Name = "Form1";
+            this.Text = "YouTube Video Downloader";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.ComboBox cmbQuality;
+        private System.Windows.Forms.Button btnDownload;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using YoutubeExplode;
+using YoutubeExplode.Common;
+using YoutubeExplode.Videos.Streams;
+
+namespace YouTubeDownloaderApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Load += Form1_Load;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Set form background color
+            this.BackColor = System.Drawing.Color.FromArgb(0, 51, 102); // Dark Blue
+
+            // Configure TextBox
+            txtUrl.BackColor = System.Drawing.Color.White;
+            txtUrl.ForeColor = System.Drawing.Color.Black;
+
+            // Configure ComboBox
+            cmbQuality.BackColor = System.Drawing.Color.White;
+            cmbQuality.ForeColor = System.Drawing.Color.Black;
+
+            // Configure Button
+            btnDownload.BackColor = System.Drawing.Color.FromArgb(102, 163, 255); // Light Blue
+            btnDownload.ForeColor = System.Drawing.Color.White;
+
+            // Configure ProgressBar
+            progressBar.BackColor = System.Drawing.Color.LightGray;
+            progressBar.ForeColor = System.Drawing.Color.Blue;
+
+            // Configure Label
+            lblStatus.ForeColor = System.Drawing.Color.White; // White text for status
+
+            // Populate the quality options
+            cmbQuality.Items.Add("144p");
+            cmbQuality.Items.Add("360p");
+            cmbQuality.Items.Add("720p");
+            cmbQuality.Items.Add("1080p");
+            cmbQuality.SelectedIndex = 0; // Default selection
+        }
+
+        private string SanitizeFileName(string fileName)
+        {
+            // Remove invalid characters from the file name
+            foreach (char c in Path.GetInvalidFileNameChars())
+            {
+                fileName = fileName.Replace(c.ToString(), "_"); // Replace with underscore
+            }
+            return fileName;
+        }
+
+        private async void btnDownload_Click(object sender, EventArgs e)
+        {
+            string url = txtUrl.Text;
+            lblStatus.Text = "Downloading...";
+            progressBar.Value = 0;
+
+            try
+            {
+                var youtube = new YoutubeClient();
+                var video = await youtube.Videos.GetAsync(url); // Use the URL directly
+
+                // Get available streams
+                var streamManifest = await youtube.Videos.Streams.GetManifestAsync(video.Id);
+                var streams = streamManifest.GetMuxedStreams().ToList(); // Accessing MuxedStreams
+
+                // Choose the selected quality
+                var selectedQuality = cmbQuality.SelectedItem.ToString();
+                var selectedStream = streams.FirstOrDefault(s => s.VideoQuality.Label == selectedQuality);
+
+                if (selectedStream != null)
+                {
+                    string sanitizedTitle = SanitizeFileName(video.Title);
+                    var filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), $"{sanitizedTitle}.{selectedStream.Container}");
+
+                    // Start the download
+                    await youtube.Videos.Streams.DownloadAsync(selectedStream, filePath, new Progress<double>(progress =>
+                    {
+                        progressBar.Value = (int)(progress * 100);
+                    }));
+
+                    lblStatus.Text = "Download completed!";
+                }
+                else
+                {
+                    lblStatus.Text = "Selected quality not available.";
+                }
+            }
+            catch (Exception ex)
+            {
+                lblStatus.Text = $"Error: {ex.Message}";
+            }
+        }
+    }
+}
